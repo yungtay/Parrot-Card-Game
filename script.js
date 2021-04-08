b2abc745e63968dd89499490bbcfc5742d88f867
Christian Yungtay <christianyungtay@poli.ufrj.br>
2021-04-08 02:34:56 -0300

    Adicionado a lógica do tempo e consertado o erro visual das cartas girando

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const cartas = ["img/bobrossparrot.gif", "img/bobrossparrot.gif", "img/explodyparrot.gif", "img/explodyparrot.gif", "img/fiestaparrot.gif", "img/fiestaparrot.gif", "img/metalparrot.gif", "img/metalparrot.gif", "img/revertitparrot.gif", "img/revertitparrot.gif", "img/tripletsparrot.gif", "img/tripletsparrot.gif", "img/unicornparrot.gif", "img/unicornparrot.gif"] ;
 const conteudoParrot = document.querySelector(".conteudo");
-let qtdcartas = (prompt("Com quantas cartas quer jogar ?"));
+let relogio ;
+let tempo = 0;
+let qtdcartas = 4//(prompt("Com quantas cartas quer jogar ?"));
 let primeiroPapagaio ;
 let segundoPapagaio ;
 let contadorJogadas = 0 ;
 let jogando = 0;
+
+
+topo = document.querySelector(".topo")
+topo.innetHTML = `PARROT CARD GAME`
 
 while (qtdcartas < 4 || qtdcartas > 14 || qtdcartas%2 !== 0){
     qtdcartas = (prompt("Com quantas cartas quer jogar ?"))
@@ -15,48 +21,54 @@ cartasDoJogo.sort(comparador)
 
 for(let i = 0; i < qtdcartas; i++){
     conteudoParrot.innerHTML += 
-    `<div class=`+`dupla-carta${i}` + `>
+    `<div class=`+`dupla-carta` + `>
         <img src="img/front.png" class="envolucro" onclick="vira(this)" alt="Carta virada pra cima">
-        <img src=${cartasDoJogo[i]} class="envolucro esconde" onclick="vira(this)" alt="Carta virada pra cima">
+        <img src=${cartasDoJogo[i]} class="envolucro vira" onclick="vira(this)" alt="Carta virada pra cima">
     </div>`
 }
+
 function vira(el){
 
-if(el.nextElementSibling !== null && jogando === 0){
-    
-    const irmao = seuirmao(el,0)
-    let escondidos = document.querySelectorAll(".esconde:last-child")
-    
-    if (escondidos.length%2 !== 0){
-        primeiroPapagaio = irmao
+    if(el.nextElementSibling !== null && jogando === 0){
+        
+        const irmao = seuirmao(el,0)
+        let escondidos = document.querySelectorAll(".vira:last-child")
+        
+        if (escondidos.length%2 !== 0){
+            primeiroPapagaio = irmao
+            if(contadorJogadas === 0){
+            relogio = setInterval(timer, 1000)
+            }
 
-    } else{
-        contadorJogadas += 1
-        segundoPapagaio = irmao
-        if(primeiroPapagaio.getAttribute('src') !== segundoPapagaio.getAttribute('src')){
-            jogando = 1
-            setTimeout(errou, 1000, primeiroPapagaio,segundoPapagaio) 
+        } else{
+            contadorJogadas += 1
+            segundoPapagaio = irmao
+            if(primeiroPapagaio.getAttribute('src') !== segundoPapagaio.getAttribute('src')){
+                jogando = 1
+                setTimeout(errou, 1000, primeiroPapagaio,segundoPapagaio) 
+            }
         }
-    }
 
-    if (document.querySelectorAll(".esconde:last-child").length === 0){
-        alert(`Você ganhou em ${contadorJogadas} jogadas!`)
-    }
-}   
+        if (document.querySelectorAll(".vira:last-child").length === 0){
+            alert(`Você ganhou em ${contadorJogadas} jogadas e em ${tempo} segundos!`)
+            clearTimeout(relogio)
+            jogarMais = prompt("Quer jogar mais uma vez ?")
+        }
+    }   
 }
 
 function seuirmao(el1,el2){
     
     if (el1 !== 0){
         const irmao = el1.nextElementSibling
-        el1.classList.toggle("esconde")
-        irmao.classList.toggle("esconde")
+        el1.classList.toggle("vira")
+        irmao.classList.toggle("vira")
         return irmao
 
     } else{
         const irmao = el2.previousElementSibling
-        el2.classList.toggle("esconde")
-        irmao.classList.toggle("esconde")
+        el2.classList.toggle("vira")
+        irmao.classList.toggle("vira")
     }   
 }
 
@@ -68,4 +80,9 @@ function errou(primeiroPapagaio,segundoPapagaio){
 
 function comparador() { 
 	return Math.random() - 0.5; 
+}
+
+function timer(){
+    tempo += 1;
+    document.querySelector(".tempo").innerHTML = tempo;
 }
